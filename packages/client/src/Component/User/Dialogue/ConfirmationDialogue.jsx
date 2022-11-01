6cf27f86655f5aac995fccc09e6874aeade50c13
pakrirama <pakrirama@gmail.com>
2022-11-01 21:04:33 +0700

    Fix Confirmation DIalogue

--- packages/client/src/Component/User/Dialogue/ConfirmationDialogue.jsx
+++ packages/client/src/Component/User/Dialogue/ConfirmationDialogue.jsx
@@ -61,7 +61,7 @@ export const ConfirmationDialogue = ({
                 colorScheme={color2 ? color2 : 'teal'}
                 onClick={() => {
                   onClose();
-                  func && id ? func(id, param) : func();
+                  func && id ? func(id, param) : null;
                   func2 && id ? func2(id) : null;
                 }}
                 ml={3}
